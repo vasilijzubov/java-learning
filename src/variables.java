522e52ab0a6d8577b2f853bda8f056ef26287708
Vasilij <vasilijs@zubovs@gmail.com>
2025-04-13 16:37:31 +0300

    [master] : homework/ Variables

---
+++ src/variables.java
@@ -0,0 +1,18 @@
+public class variables {
+    public static void main(String[] args) {
+        byte byteNumbers = 123;
+        short shortNumber = 25900;
+        int intNumber = 650-300;
+        long longNumber = 11110*11000L;
+        float floatNumber = 43023F;
+        double myDoubleNumber = 328.54;
+        boolean myBooleanNumber = true;
+        char myCharNumber = 'C';
+
+
+
+
+    }
+
+}
+
